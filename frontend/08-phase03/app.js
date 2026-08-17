74f418eff2332dd554febf932578e5bd676f3a4c
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-17 17:43:18 +0530

    added phase 03

---
+++ frontend/08-phase03/app.js
@@ -0,0 +1,18 @@
+//hoisting
+console.log(x)
+var x = 10;
+console.log(x)
+
+hello();
+function hello(){
+    console.log("hello");
+}
+
+// console.log(age)
+// let age = 20;    //tdz
+
+// Part 2: Loops
+//Q6. Print numbers from 1 to 50 using a loop.
+for(let i=1; i <= 50; i++){
+    console.log(i)
+}
