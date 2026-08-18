1ec1d2003346794bcc8a7a36134a6996fa56ddb4
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-18 12:05:03 +0530

    added phase 05

--- frontend/10-phase05/app.js
+++ frontend/10-phase05/app.js
@@ -1,1 +1,13 @@
 console.log('synchronous')
+
+//Synchronous => order me work karta h
+console.log("Start")
+let a = 42;
+let b = 23;
+let c = a + b;
+function add(){
+    console.log(c)
+}
+add();
+
+console.log("End")
