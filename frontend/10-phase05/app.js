a043709f84d2db70146b30e7ed2a929b3489400b
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-18 23:16:51 +0530

    added phase 05

--- frontend/10-phase05/app.js
+++ frontend/10-phase05/app.js
@@ -1,6 +1,6 @@
 console.log('synchronous')
 
-//Synchronous => order me work karta h
+ //Synchronous => order me work karta h
 console.log("Start")
 let a = 42;
 let b = 23;
@@ -23,3 +23,40 @@ setTimeout(function (){
 setTimeout(() => {
       console.log(3)
 },3000);
+
+var api = fetch('https://jsonplaceholder.typicode.com/todos/1')
+console.log(api)
+
+// promise bnaya
+let p1 = new Promise((resolve, reject)=>{
+    let val = false
+    if(val){
+        console.log("true")
+    }
+    else{
+        console.log("false")
+    }
+})
+
+function orderFood(){
+    let myOrdor = new Promise(function (resolve, reject){
+        console.log("your order is comming");
+        let orderStatus = true
+        setTimeout(function(){
+            if(orderStatus){
+                console.log("DElivery wale bhaiya aa gye hai")
+                resolve()
+            }
+            else{
+                console.log("Delivery faild")
+                reject()
+             }
+ },3000)
+   })
+   myOrdor.then(function(){
+        console.log("now payment")
+    }).catch(function(){
+        console.log("complain karo")
+    })
+    }
+orderFood()
