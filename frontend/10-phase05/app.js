48dfb5b9b36a6f0a5bea8a2b36710ba98f11aa30
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-19 15:49:21 +0530

    added phase 05

--- frontend/10-phase05/app.js
+++ frontend/10-phase05/app.js
@@ -27,7 +27,7 @@ setTimeout(() => {
 var api = fetch('https://jsonplaceholder.typicode.com/todos/1')
 console.log(api)
 
-// promise bnaya
+ // promise bnaya
 let p1 = new Promise((resolve, reject)=>{
     let val = false
     if(val){
@@ -60,3 +60,61 @@ function orderFood(){
     })
     }
 orderFood()
+
+// 1. Timer Functions
+
+// JavaScript mein timer functions ka use kisi code ko turant execute na karke kuch time baad execute karne ke liye hota hai.
+// set timeout => Kisi function ko given time ke baad ek baar chalata hai.
+setTimeout(()=>{
+    console.log("it's set timeout")   // yaha 1 sec ke bad print hoga
+},1000)
+
+ //setInterwal => Kisi function ko given interval ke baad repeatedly chalata hai.
+setInterval(function(){
+    console.log("set Interwal") // yaha har 2 second ke bad cod chalata rhega mtlb repeatedly
+},2000)
+
+// isi repetition ko stop karne ke liye ham clear interwal user karte hai
+let id = setInterval(function(){
+    console.log("stop! set interwal")
+},2000)
+clearInterval(id);
+
+//Q2. setInterval() use karke har 1 second mein counter print karo:
+// 1 → 2 → 3 → 4 → 5
+// 5 ke baad interval stop hona chahiye.
+let count = 1
+let counter = setInterval(function(){
+       console.log("counting")
+      if(count ===5){
+        clearInterval(counter)
+      }
+        count++;
+},1000)
+
+
+// Q1.Ek Promise banao jisme:
+
+// orderStatus = true → "Order Successful"
+// orderStatus = false → "Order Failed"
+
+// .then() aur .catch() se handle karo.
+let p2 = new Promise(function(resolve, reject){
+    let orderStatus = false
+    if(orderStatus){ 
+  console.log("Order successfull")
+    resolve()
+}
+else{
+    console.log("Order failed")
+    reject()
+}
+  
+})
+p2.then(function(){
+    console.log("now payment")
+}).catch(function(){
+    console.log("sorry for that we could not reach out due to out of access your address")
+}).finally(()=>{
+    console.log("process completed")
+})
