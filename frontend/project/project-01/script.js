d5c4b0dc944d31705000fb7125f06af50de00d5b
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-26 12:32:28 +0530

    added project on bul

--- frontend/project/project-01/script.js
+++ frontend/project/project-01/script.js
@@ -1,1 +1,30 @@
-console.log("bulb")
+const bulb = document.querySelector(".bulb")
+const btn = document.querySelector("button")
+
+// method first
+
+let  flag = true
+btn.addEventListener("click", ()=>{
+   if(flag){
+      bulb.style.backgroundColor = 'yellow'
+      btn.innerHTML="off"
+      console.log("i am on mode")
+      flag = false
+   }
+   else{
+     bulb.style.backgroundColor = 'transparent'
+     btn.innerHTML="on"
+     console.log("i am off mode")
+     flag = true
+   }
+})
+// method second
+btn.addEventListener("click",function(){
+   if( bulb.classList.toggle("lightup")) {
+    btn.textContent = "off"
+   }
+   else{
+    btn.textContent = "on"
+   }
+    
+})
