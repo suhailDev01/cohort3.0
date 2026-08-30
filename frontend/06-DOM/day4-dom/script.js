23ff328c12926d2282951f3523f6ee07f564ccf7
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-30 12:24:08 +0530

    added dom 4

--- frontend/06-DOM/day4-dom/script.js
+++ frontend/06-DOM/day4-dom/script.js
@@ -1,6 +1,9 @@
 const body= document.body
 const main = document.querySelector("main")
 const btn = document.querySelector("button")
+const form = document.querySelector("form")
+const inp = document.querySelector("#name")
+const inp1 = document.querySelector("#email")
 
 main.addEventListener("click", (events)=>{
    // console.log(events)
@@ -21,3 +24,11 @@ btn.addEventListener("click", (events)=>{
    // console.log(events)
     console.log("div triggred")
 },true)
+
+form.addEventListener("submit",(events)=>{
+    events.preventDefault()
+   let name = inp.value  
+   let email = inp1.value
+   console.log(name, email)
+   form.reset()
+})
