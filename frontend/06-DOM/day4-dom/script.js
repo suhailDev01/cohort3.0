cd24a6a7f285d9a93228a49e3417e431815171f3
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-30 12:01:42 +0530

    added dom 4

--- frontend/06-DOM/day4-dom/script.js
+++ frontend/06-DOM/day4-dom/script.js
@@ -1,1 +1,23 @@
-console.log("events")
+const body= document.body
+const main = document.querySelector("main")
+const btn = document.querySelector("button")
+
+main.addEventListener("click", (events)=>{
+   // console.log(events)
+    console.log(events)
+})
+
+  //EVENT TRAVERSAL OR EVENT  PROPOGATION
+//before captutre true or true lagne se ye event bubbling mtlb bottom to top me chalta h aftre true event capturing mtlb top to bottom
+body.addEventListener("click", (events)=>{
+   // console.log(events)
+    console.log("body triggred")
+},true)
+main.addEventListener("click", (events)=>{
+   // console.log(events)
+    console.log("main triggred")
+},true)
+btn.addEventListener("click", (events)=>{
+   // console.log(events)
+    console.log("div triggred")
+},true)
