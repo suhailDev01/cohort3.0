87b9a1b0343ab39b311fec87fd1b46ce35173d7b
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-31 13:28:47 +0530

    added project 03

--- frontend/project/project-03/script.js
+++ frontend/project/project-03/script.js
@@ -1,1 +1,22 @@
-console.log("project")
+const main = document.querySelector("main")
+const box = document.querySelector(".box")
+const btn = document.querySelector("button")
+const timer = document.querySelector("#timer")
+let time = 0;
+let interwal;
+btn.addEventListener("click", ()=>{
+      clearInterval(interwal);
+    interwal = setInterval(() => {
+        time += 1;
+        timer.textContent = time;
+           let rx = Math.random() *100;
+           let ry = Math.random() * 100;
+           box.style.top = `${rx}%`;
+           box.style.right = `${ry}%`;
+        }, 1000);
+
+        setTimeout (()=>{
+     clearInterval(interwal)
+      },10000)
+
+  })
