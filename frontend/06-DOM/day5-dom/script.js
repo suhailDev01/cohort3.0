93a27e57249d7736cba490aa3159d086af3d07c8
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-08-31 10:40:56 +0530

    event delegation

--- frontend/06-DOM/day5-dom/script.js
+++ frontend/06-DOM/day5-dom/script.js
@@ -1,1 +1,9 @@
-console.log("day five ")
+const five = document.querySelector("#five")
+const main = document.querySelector("main")
+
+// five.addEventListener("click", (event)=>{
+//     console.log(event.target)
+// })
+main.addEventListener("click", (event)=>{
+    console.log(event.target)
+})
