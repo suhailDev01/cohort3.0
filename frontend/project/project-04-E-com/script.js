f89fe3fc15801f05831d1569ae859f661543457e
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-01 13:28:59 +0530

    added prjoect 04

--- frontend/project/project-04-E-com/script.js
+++ frontend/project/project-04-E-com/script.js
@@ -1,1 +1,6 @@
-console.log("project E com")
+const createBtn = document.querySelector("#create")
+const formDiv = document.querySelector(".form")
+
+createBtn.addEventListener("click",()=>{
+    formDiv.style.display = "flex"
+})
