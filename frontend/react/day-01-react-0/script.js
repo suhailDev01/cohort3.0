f3987d36af0de9362585e9d353acfc4da233e32a
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-07 12:43:15 +0530

    added react 0

--- frontend/react/day-01-react-0/script.js
+++ frontend/react/day-01-react-0/script.js
@@ -1,10 +1,15 @@
 
 // console.log(window)
 // console.log(React)
+
+//real dom
 const h1 = document.createElement("h1");
 h1.textContent = "hello , I am real dom"
 document.body.append(h1)
 console.log("real dom ->", h1)
-const rh1 = React.createElement("h1")
+//virtual dom
+const rh1 = React.createElement("h1",
+    null,
+    React.createElement("span",{}, "I am under h1"))
 
 console.log("virtual dom ->", rh1)
