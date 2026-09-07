e2df606e648dcf66b63b6ade6b8ce40fb1967172
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-07 12:19:19 +0530

    added react 0

--- frontend/react/day-01-react-0/script.js
+++ frontend/react/day-01-react-0/script.js
@@ -1,1 +1,10 @@
-console.log(window)
+
+// console.log(window)
+// console.log(React)
+const h1 = document.createElement("h1");
+h1.textContent = "hello , I am real dom"
+document.body.append(h1)
+console.log("real dom ->", h1)
+const rh1 = React.createElement("h1")
+
+console.log("virtual dom ->", rh1)
