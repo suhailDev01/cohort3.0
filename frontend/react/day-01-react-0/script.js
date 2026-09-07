968d3c625fa0d1a73c6878deca15a7e17e5dbd5b
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-07 14:01:46 +0530

    added react0

--- frontend/react/day-01-react-0/script.js
+++ frontend/react/day-01-react-0/script.js
@@ -3,21 +3,23 @@
 // console.log(React)
 
 //real dom
-// const h1 = document.createElement("h1");
-// h1.textContent = "hello"
-// document.body.append(h1)
-// console.log("real dom ->", h1)
-// //virtual dom
-// const rh1 = React.createElement("h1",
-//     null,
-//     React.createElement("span",{}, "I am under h1"))
+const h1 = document.createElement("h1");
+h1.textContent = "hello"
+document.body.append(h1)
+console.log("real dom ->", h1)
 
-// console.log("virtual dom ->", rh1)
+//virtual dom / ReactDOM
+const rh1 = React.createElement("h1",
+    null,
+    React.createElement("span",{}, "I am under h1"))
 
-// let realdom = document.querySelector("#root")
-// let reactOfRoot = ReactDOM.createRoot(realdom)
-// reactOfRoot.render(rh1)
-// //console.log(ReactDOM);
+console.log("virtual dom ->", rh1)
+
+let realdom = document.querySelector("#root")
+let reactOfRoot = ReactDOM.createRoot(realdom)
+reactOfRoot.render(rh1)
+
+ //console.log(ReactDOM);
 import {a,sum} from "./main.js"
 
 console.log(a)
