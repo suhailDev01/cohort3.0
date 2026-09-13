d991c3c3f191a0fc512d353d65d286d99cfd7fdc
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-13 12:23:03 +0530

    added css manualy

--- frontend/react/day-5-state-dataRendring/src/App.jsx
+++ frontend/react/day-5-state-dataRendring/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./App.css"
+
 const App = () =>{
  return(
    <div>
-    <h1>Hello Developer</h1>
+    <h1 className="red">Hello Developer</h1>
   </div>
  )
   
