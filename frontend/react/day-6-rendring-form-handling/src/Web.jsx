b0eff5f284a1d5e6d9ce0b95887c92b949ff5326
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-16 16:27:25 +0530

    added form handling approach

--- frontend/react/day-6-rendring-form-handling/src/Web.jsx
+++ frontend/react/day-6-rendring-form-handling/src/Web.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react'
 
 const Web = () => {
-    const [name, setName] =useState("")
-    console.log(name)
-    const [email, setEmail] =useState("")
-    console.log(email)
-    const [password, setPassword] =useState("")
-    console.log(name)
+    const [formData, setformData] =useState({
+        name:"",
+        email:"",
+        password:""
+    })
+    console.log(formData)
+    // const [email, setEmail] =useState("")
+    // console.log(email)
+    // const [password, setPassword] =useState("")
+    // console.log(name)
   return (
     <div className='flex flex-col gap-5 w-70 m-2'>
         <input onChange={(e)=>{
-            setName(e.target.value)   }}
+            setformData(e.target.value)   }}
          className='border-2' type='text' placeholder='Name'/>
-        <input onChange={(e)=>{
-            setEmail(e.target.value)
-        }} className='border-2' type='text' placeholder='Email'/>
+        <input
+        //  onChange={(e)=>{
+        //     setEmail(e.target.value) }}
+             className='border-2' type='text' placeholder='Email'/>
        
-        <input onChange={(e)=>{
-            setPassword(e.target.value)
-        }} className='border-2' type='text' placeholder='Paasword'/>
+        <input 
+        // onChange={(e)=>{
+        //     setPassword(e.target.value) }} 
+            className='border-2' type='text' placeholder='Paasword'/>
             <button className='border-2'>Submit</button>
         <h1>this is name- {name}</h1>
          <h1>this is email- {email}</h1>
