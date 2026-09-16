5bc3e7df9216a80aa31ae8eafad7a1dc1602b4ad
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-16 14:49:20 +0530

    added brutoforce algorithm for form handling

--- frontend/react/day-6-rendring-form-handling/src/Web.jsx
+++ frontend/react/day-6-rendring-form-handling/src/Web.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Web = () => {
+    const [name, setName] =useState("")
+    console.log(name)
+    const [email, setEmail] =useState("")
+    console.log(email)
+    const [password, setPassword] =useState("")
+    console.log(name)
   return (
-    <div>
-        <input type='text' placeholder='Name'/>
-        <input type='text' placeholder='Email'/>
-        <input type='text' placeholder='Paasword'/>
-        <button>Submit</button>
+    <div className='flex flex-col gap-5 w-70 m-2'>
+        <input onChange={(e)=>{
+            setName(e.target.value)   }}
+         className='border-2' type='text' placeholder='Name'/>
+        <input onChange={(e)=>{
+            setEmail(e.target.value)
+        }} className='border-2' type='text' placeholder='Email'/>
+       
+        <input onChange={(e)=>{
+            setPassword(e.target.value)
+        }} className='border-2' type='text' placeholder='Paasword'/>
+            <button className='border-2'>Submit</button>
+        <h1>this is name- {name}</h1>
+         <h1>this is email- {email}</h1>
+        <h1>this is password - {password}</h1>
         
     </div>
   )
