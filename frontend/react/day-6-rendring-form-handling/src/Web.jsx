fe0812dea85acae92e7ebd2290ed8c8b6dccc5eb
Suhail  Ahamad <suhailkhan86044048@gmail.com>
2026-09-16 13:16:41 +0530

    added day-6

---
+++ frontend/react/day-6-rendring-form-handling/src/Web.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Web = () => {
+  return (
+    <div>
+        <input type='text' placeholder='Name'/>
+        <input type='text' placeholder='Email'/>
+        <input type='text' placeholder='Paasword'/>
+        <button>Submit</button>
+        
+    </div>
+  )
+}
+
+export default Web
--- frontend/react/day-6-rendring-form-handling/src/main.jsx
+++ frontend/react/day-6-rendring-form-handling/src/main.jsx
@@ -2,5 +2,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Web from './Web.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(<Web />)
